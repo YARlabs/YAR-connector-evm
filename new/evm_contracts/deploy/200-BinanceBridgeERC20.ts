fca8dee5cce7b84c17d45784b4ce690602def791
tonatero <tonatero@proton.me>
2023-03-20 17:10:38 +0300

    contracts add EthereumBridgeERC20 deploy script

---
+++ new/evm_contracts/deploy/200-BinanceBridgeERC20.ts
@@ -0,0 +1,35 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+import { DeployFunction } from 'hardhat-deploy/types'
+
+const deploy: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { ethers, deployments } = hre
+  const { deploy, get } = deployments
+
+  const signers = await ethers.getSigners()
+  const deployer = signers[0]
+  const validator = signers[1]
+
+  const currentChain = ethers.utils.keccak256('BINANCE')
+  const isProxyChain = true
+  const registeredChains = [
+    ethers.utils.keccak256('POLYGON'),
+    ethers.utils.keccak256('YAR'),
+    ethers.utils.keccak256('ETHEREUM'),
+  ]
+  const IssuedERC20Deployment = await get('IssuedERC20')
+
+  const deployment = await deploy('BinanceBridgeERC20', {
+    contract: 'BridgeERC20',
+    from: deployer.address,
+    args: [
+      currentChain, // _currentChain,
+      isProxyChain, // _isProxyChain,
+      registeredChains, // _registeredChains,
+      IssuedERC20Deployment.address, // _issuedTokenImplementation,
+      validator.address, // _validator
+    ],
+  })
+}
+
+deploy.tags = ['BinanceBridgeERC20']
+export default deploy
---
+++ new/evm_contracts/deploy/200-EthereumBridgeERC20.ts
@@ -0,0 +1,35 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+import { DeployFunction } from 'hardhat-deploy/types'
+
+const deploy: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { ethers, deployments } = hre
+  const { deploy, get } = deployments
+
+  const signers = await ethers.getSigners()
+  const deployer = signers[0]
+  const validator = signers[1]
+
+  const currentChain = ethers.utils.keccak256('ETHEREUM')
+  const isProxyChain = true
+  const registeredChains = [
+    ethers.utils.keccak256('POLYGON'),
+    ethers.utils.keccak256('YAR'),
+    ethers.utils.keccak256('BINANCE'),
+  ]
+  const IssuedERC20Deployment = await get('IssuedERC20')
+
+  const deployment = await deploy('EthereumBridgeERC20', {
+    contract: 'BridgeERC20',
+    from: deployer.address,
+    args: [
+      currentChain, // _currentChain,
+      isProxyChain, // _isProxyChain,
+      registeredChains, // _registeredChains,
+      IssuedERC20Deployment.address, // _issuedTokenImplementation,
+      validator.address, // _validator
+    ],
+  })
+}
+
+deploy.tags = ['EthereumBridgeERC20']
+export default deploy
