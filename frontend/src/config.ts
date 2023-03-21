dd27c6be6ae810b50470a355d30b51c63ef97440
tonatero <tonatero@proton.me>
2023-03-21 13:03:12 +0300

    change rpcs

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,15 +1,15 @@
 export const config = {
   network: {
     goerli: {
-      url: "https://endpoints.omniatech.io/v1/eth/goerli/public",
+      url: "https://goerli.infura.io/v3/9502027cfe6040b3a3a388b07fcf2638",
       accounts: [process.env.GOERLI_PRIV_KEY],
     },
     mumbai: {
-      url: "https://rpc-mumbai.maticvigil.com/",
+      url: "https://matic-mumbai.chainstacklabs.com",
       accounts: [process.env.MUMBAI_PRIV_KEY],
     },
     bsctestnet: {
-      url: "https://data-seed-prebsc-2-s3.binance.org:8545",
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
     },
     yar: {
       url: "https://rpc1.testnet.yarchain.org",
