76b34c2febb2fdd43404776f303d33f0a660852e
tonatero <tonatero@proton.me>
2023-04-05 00:15:34 +0300

    update config

--- configs/validator.config.production.ts
+++ configs/validator.config.production.ts
@@ -4,26 +4,32 @@ export default {
     {
       name: "YAR",
       rpcUrl: "https://rpc1.testnet.yarchain.org",
-      address: "0x5583B9dA5Dd838f604aD2A3e2979bD6D91482967",
+      address: "0x77d2aB62BF4B733726C0b757D53449770FE42e82",
       privateKey: process.env.YAR_VALIDATOR_PRIVATE_KEY,
     },
     {
       name: "POLYGON",
       rpcUrl: "https://rpc-mumbai.maticvigil.com",
-      address: "0x3c755d71755a70499F38154c6c14E99C6D52D8b9",
+      address: "0x4fc4d6C93641aF34d4158fAcd1fe51A526d5Bf93",
       privateKey: process.env.POLYGON_VALIDATOR_PRIVATE_KEY,
     },
     {
       name: "BINANCE",
       rpcUrl: "https://data-seed-prebsc-1-s1.binance.org:8545",
-      address: "0x1782eC0562b79B94b52a13A24328a9981a04E635",
+      address: "0x8d77CCe6F321F36E1a7179636b40E100A6202C69",
       privateKey: process.env.BINANCE_VALIDATOR_PRIVATE_KEY,
     },
     {
       name: "ETHEREUM",
       rpcUrl: "https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
-      address: "0x35DF543CCaCF7D728cD3dA60F8d37088D00640d5",
+      address: "0xe944eDe345139EC64d27059A9A5006A2ce367326",
       privateKey: process.env.ETHEREUM_VALIDATOR_PRIVATE_KEY,
+    },
+    {
+      name: "SKALE",
+      rpcUrl: "https://staging-v3.skalenodes.com/v1/staging-fast-active-bellatrix",
+      address: "0xA4a52100c26308f8Df26873635Bd5600Be56938B",
+      privateKey: process.env.SKALE_VALIDATOR_PRIVATE_KEY,
     },
   ],
 };
