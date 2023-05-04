909c257a955680a308c20e511dcb3e7afc584d0a
tonatero <tonatero@proton.me>
2023-05-04 09:04:13 +0300

    hide image, json 721

--- apps/frontend_erc721/src/tabs/main-tab/index.tsx
+++ apps/frontend_erc721/src/tabs/main-tab/index.tsx
@@ -347,40 +347,45 @@ const MainTab = () => {
             ) : null}
           </div>
         </div>
-        <div className="wizard-navigation">
-          <ul className="nav nav-pills" >
-            <li><a href="#image" data-toggle="tab">image</a></li>
-            <li><a href="#json" data-toggle="tab">json</a></li>
-          </ul> 
-        </div>  
-        <div className="tab-content" style={{minHeight: "0px"}}>
-          <div className="tab-pane" id="image">
-            {
-              imageLink &&
-              <div style={{width: "100%" ,display: "flex", justifyContent: "center"}}>
-                <img src={imageLink} alt=""></img>
-              </div>
-            }
-          </div>
-          <div className="tab-pane" id="json">
-          <SyntaxHighlighter language="json" showLineNumbers style={docco}>
-            { JSON.stringify(jsonMetadata, undefined, 4) }
-          </SyntaxHighlighter>
+        <div style={{ visibility: Object.keys(jsonMetadata).length !== 0 ? "visible" : "hidden",
+                      height: Object.keys(jsonMetadata).length !== 0 ? "max-content" : "0" }} >
+          <div className="wizard-navigation">
+            <ul className="nav nav-pills" >
+              <li><a href="#image" data-toggle="tab">image</a></li>
+              <li><a href="#json" data-toggle="tab">json</a></li>
+            </ul> 
+          </div>  
+          <div className="tab-content" style={{minHeight: "0px"}}>
+            <div className="tab-pane" id="image">
+              {
+                imageLink &&
+                <div style={{width: "100%" ,display: "flex", justifyContent: "center"}}>
+                  <img src={imageLink} alt=""></img>
+                </div>
+              }
+            </div>
+            <div className="tab-pane" id="json">
+              <SyntaxHighlighter language="json" showLineNumbers style={docco}>
+                { JSON.stringify(jsonMetadata, undefined, 4) }
+              </SyntaxHighlighter>
+            </div>
           </div>
         </div>
         <div className="wizard-footer">
-          <div className="pull-right">
-            <button
-              type="button"
-              className="btn btn-fill btn-danger btn-wd"
-              value="Transfer"
-              onClick={() => transferToBridge()}
-              disabled={isDisable}
-              style={{ height: "42px" }}
-            >
-              {isDisable ? <div className="spinner"></div> : "Transfer"}
-            </button>
-          </div>
+          { uri &&
+            <div className="pull-right">
+              <button
+                type="button"
+                className="btn btn-fill btn-danger btn-wd"
+                value="Transfer"
+                onClick={() => transferToBridge()}
+                disabled={isDisable}
+                style={{ height: "42px" }}
+              >
+                {isDisable ? <div className="spinner"></div> : "Transfer"}
+              </button>
+            </div>
+          }
           <div className="clearfix"></div>
         </div>
       </div>
