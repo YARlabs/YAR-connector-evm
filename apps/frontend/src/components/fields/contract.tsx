91f2c073fc0cb9903283375f129dcd0748c96329
tonatero <tonatero@proton.me>
2023-11-07 15:17:41 +0000

    upd erc20 frontend

--- apps/frontend/src/components/fields/contract.tsx
+++ apps/frontend/src/components/fields/contract.tsx
@@ -1,10 +1,18 @@
 import { useGetTicker } from "../../hooks/useGetTicker";
 import { useTypedSelector } from "../../hooks/useTypedSelector";
 import { useActions } from "../../hooks/useActions";
+import { useState } from "react";
+
+
+const tickers = {
+
+}
 
 const ContractField = () => {
   const { chainIdFrom, token } = useTypedSelector((state) => state.main);
   const { SetTicker, ClearTicker, SetToken } = useActions();
+
+  const [type, setType] = useState("erc20");
 
   const tickerHook = useGetTicker();
   const setContract = async (address: string) => {
@@ -16,23 +24,49 @@ const ContractField = () => {
       ClearTicker();
     }
   };
+
+  const setContractType = async (type: "native" | "erc20") => {
+    if (type == "native") {
+      setType("native");
+      setContract("0x0000000000000000000000000000000000000000");
+    } else if (type == "erc20") {
+      setType("erc20");
+      setContract("");
+    }
+  };
+
   return (
     <>
       <div className="col-sm-12">
-        <div className="form-group label-floating">
-          <label className="control-label">Contract:</label>
-          <input
-            type="text"
-            className="form-control"
-            defaultValue={token}
-            onChange={(e) => setContract(e.target.value)}
-          />
-          <div>
-            {" "}
-            (pass 0x0000000000000000000000000000000000000000 to transfer native
-            token from original chain)
+        <div>
+          Token type:<span> </span>
+          <div
+            className="btn btn-xs"
+            onClick={() => setContractType("erc20")}
+            style={{backgroundColor: type == 'erc20' ? '#f44336' : '#999999'}}
+          >
+            Contract
+          </div>
+
+          <div
+            className="btn btn-xs"
+            onClick={() => setContractType("native")}
+            style={{backgroundColor: type == 'native' ? '#f44336' : '#999999'}}
+          >
+            Native
           </div>
         </div>
+        {type == "erc20" && (
+          <div className="form-group label-floating">
+            <label className="control-label">Contract:</label>
+            <input
+              type="text"
+              className="form-control"
+              defaultValue={token}
+              onChange={(e) => setContract(e.target.value)}
+            />
+          </div>
+        )}
       </div>
     </>
   );
--- apps/frontend/src/hooks/useGetTicker.ts
+++ apps/frontend/src/hooks/useGetTicker.ts
@@ -3,11 +3,26 @@ import { idToRpcUrl } from "../utils/idToChainName";
 import { providers } from "ethers";
 import { IERC20Metadata__factory } from "typechains";
 
+const nativeTikers: any = {
+    10226688: 'YAR',
+    80001: 'MATIC',
+    97: 'BNB',
+    5: 'ETH',
+    476158412: 'SKL',
+    420: 'OP',
+    421613: 'ARB',
+    43113: 'AVAX',
+    84531: 'BASE',
+}
 
 export const useGetTicker = () => {
 
     return useCallback(
         async (token: string, chainId: number) => {
+            if(token == '0x0000000000000000000000000000000000000000') {
+                return nativeTikers[chainId] ?? '-';
+            }
+
             const provider = new providers.JsonRpcProvider(idToRpcUrl[chainId]);
             const contractToken = IERC20Metadata__factory.connect(token, provider);
             try {
